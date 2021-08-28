8fbbedea41e2dd05c4f41b34fae2ea80fed8ec88
Yakov Rakhamimov <jacobra19@gmail.com>
2021-08-29 01:59:59 +0300

    inital commit and html loading infa

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function App() {
+    let iframeElement: HTMLIFrameElement | null = null
+    const handleNav = (page: string) => {
+        try {
+            iframeElement = document.getElementById(
+                'main-iframe',
+            ) as HTMLIFrameElement
+            if (iframeElement) {
+                iframeElement.src = `./src/projects/${page}/index.html`
+            }
+        } catch (error) {
+            console.log('error :>> ', error)
+        }
+    }
+    return (
+        <div className="App">
+            <header
+                style={{
+                    height: 100,
+                    width: '100%',
+                    backgroundColor: 'yellow',
+                }}
+            >
+                javascript 30
+                <select
+                    onChange={(e) => handleNav(e.target.value)}
+                    defaultValue={''}
+                >
+                    {' '}
+                    <option disabled value={''}>
+                        {' '}
+                        -- select an option --{' '}
+                    </option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                </select>
+            </header>
+        </div>
+    )
+}
+
+export default App
---
+++ src/projects/01/script.js
@@ -0,0 +1,4 @@
+prompt("Hello, World!");
+function onDrumClick(params) {
+    alert("Hello, Click!");
+}
